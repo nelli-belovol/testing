a79067ddd3b7f13ffa5da90e10ac951b1a72ec10
nelli-belovol <nelli199020@gmail.com>
2024-07-19 14:55:09 +0300

    watch and debug

--- src/filterArray.js
+++ src/filterArray.js
@@ -1,10 +1,7 @@
 export function filterArray(arr, callback) {
   const newArray = [];
   for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-    if (callback(arr[i])) {
-      newArray.push(arr[i]);
-    }
+    if (callback(arr[i])) newArray.push(arr[i]);
   }
   return newArray;
 }
